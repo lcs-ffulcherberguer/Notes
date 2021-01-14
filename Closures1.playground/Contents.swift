226cab60fdc3de18e2f142ab4f19bfeff2607f96
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-01-14 17:00:35 -0500

    Closures and Parameters, how to pass closures into functions.

--- Closures1.playground/Contents.swift
+++ Closures1.playground/Contents.swift
@@ -27,15 +27,31 @@ import Cocoa
 //it has to be added inside your closure
 //before the in keyword
 
-let driving = { (place: String) in
-    print("I'm going to \(place) in my car")
-}
+///let driving = { (place: String) in
+    ///print("I'm going to \(place) in my car")
+///}
 
 //Use string -> before in
 //if you want a closure that returns a string
-let drivingWithReturn = { (place: String) -> String in
-    return "I'm going to \(place) in my car"
+///let drivingWithReturn = { (place: String) -> String in
+    ///return "I'm going to \(place) in my car"
+///}
+
+///let message = drivingWithReturn("London")
+///print(message)
+
+
+
+//CLOSURES AS PARAMETERS
+//You can pass closures into functions
+
+let driving = {
+    print("I'm driving in my car")
 }
 
-let message = drivingWithReturn("London")
-print(message)
+//specify the parameter type as () -> Void. That means “accepts no parameters, and returns Void”
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
