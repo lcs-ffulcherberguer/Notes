0c9b24061ee8aaacef808a4f7067878294d229ad
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-01-14 12:14:42 -0500

    Learned about Accepting parameters.

--- Closures1.playground/Contents.swift
+++ Closures1.playground/Contents.swift
@@ -3,8 +3,19 @@ import Cocoa
 //CREATING BASIC CLOSURES
 //you can create a function and assing it to a variable
 
-let driving = {
-    print("I'm driving in my car")
+///let driving = {
+    ///print("I'm driving in my car")
+///}
+
+///driving()
+
+
+//ACCEPTING PARAMETERS IN CLOSURE
+//Parameters are listed inside the open braces
+
+let driving = { (place: String) in
+    print("I'm going to \(place) in my car")
 }
 
-driving()
+//you don't need parameter labels when running closures
+driving("London")
