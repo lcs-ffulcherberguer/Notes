f1d6585c2bc4ce0f03f7992ddf673c0d1b697c19
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-01-18 20:33:13 -0500

    Property observers

--- Struc1.playground/Contents.swift
+++ Struc1.playground/Contents.swift
@@ -20,16 +20,39 @@ import Cocoa
 //another stored property was added to the Sport struct
 // then a computed property
 
-struct Sport {
-    var name: String
-    var isOlympicSport: Bool
+///struct Sport {
+    ///var name: String
+    ///var isOlympicSport: Bool
 
-    var olympicStatus: String {
-        if isOlympicSport {
-            return "\(name) is an Olympic sport"
-        } else {
-            return "\(name) is not an Olympic sport"
+    ///var olympicStatus: String {
+        ///if isOlympicSport {
+            ///return "\(name) is an Olympic sport"
+        ///} else {
+            ///return "\(name) is not an Olympic sport"
+        ///}
+    ///}
+///}
+
+
+
+//PROPERTY OBSERVERS
+//Property observers let you run code before or after any property changes
+// This will run some code every time amount changes:
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
         }
     }
 }
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+  
+    
 
