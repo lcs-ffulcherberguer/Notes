5eea5aa0c02bdc74783ef5d6f020ebf79dfbd2a4
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-01-18 20:18:35 -0500

    Computed Properties

--- Struc1.playground/Contents.swift
+++ Struc1.playground/Contents.swift
@@ -3,12 +3,33 @@ import Cocoa
 //CREATING STRUCTS
 //this is a struct with one property
 //this defines the type
-struct Sport {
-    var name: String
-}
+///struct Sport {
+    ///var name: String
+///}
 
 //made both name and tennis variable
-var tennis = Sport(name: "Tennis")
-print(tennis.name)
+///var tennis = Sport(name: "Tennis")
+///print(tennis.name)
 
-tennis.name = "Lawn tennis"
+///tennis.name = "Lawn tennis"
+
+
+
+
+//COMPUTED PROPERTIES
+//another stored property was added to the Sport struct
+// then a computed property
+
+struct Sport {
+    var name: String
+    var isOlympicSport: Bool
+
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
