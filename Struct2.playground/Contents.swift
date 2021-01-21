3ed1e3720d0023a067b4db11e04cbae33ee5e511
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-01-20 19:55:22 -0500

    Lazy Properties

--- Struct2.playground/Contents.swift
+++ Struct2.playground/Contents.swift
@@ -30,16 +30,47 @@ import Cocoa
 //'self' points to whatever instance of the struct is currently being used.
 //useful when you create initializers that have the same parameter names as your property.
 
+///struct Person {
+    ///var name: String
+
+    ///init(name: String) {
+        ///print("\(name) was born!")
+        ///self.name = name
+   ///}
+///}
+
+
+
+
+//LAZY PROPERTIES
+//Swift lets you create some properties only when they are needed.
+
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+
+//We might use that FamilyTree struct as a property inside a Person struct
+
 struct Person {
     var name: String
 
+    //add the lazy keyword to the familyTree property
+    //Swift will only create the FamilyTree struct when it’s first accessed
+
+    lazy var familyTree = FamilyTree()
+    
     init(name: String) {
-        print("\(name) was born!")
         self.name = name
-   }
+    }
 }
 
+var ed = Person(name: "Ed")
 
+//you need to access the property at least once
+//if you want to see the “Creating family tree!” message
 
+ed.familyTree
 
 
