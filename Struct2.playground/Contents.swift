399c825495f83a7b5fbd302c2a82fac7bce16b89
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-01-20 19:40:50 -0500

    Initializers.

---
+++ Struct2.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Cocoa
+ 
+//INITIALIZERS
+//provide different ways to create your struct.
+//User struct that has one property
+
+///struct User {
+    ///var username: String
+///}
+
+//When creating a struct, must provide a username
+
+///var user = User(username: "twostraws")
+
+///struct user {
+
+    ///init() {
+        ///username = "Anonymous"
+        ///print("Creating a new user!")
+    ///}
+///}
+
+///var user = User()
+///user.username = "twostraws"
+
+
