a8fd160cdd9c7acd1a401ce610403ef98c6d20f2
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2021-01-20 19:49:21 -0500

    Referring to the current instance.

--- Struct2.playground/Contents.swift
+++ Struct2.playground/Contents.swift
@@ -24,3 +24,22 @@ import Cocoa
 ///user.username = "twostraws"
 
 
+
+
+//Referring to the urrent instance
+//'self' points to whatever instance of the struct is currently being used.
+//useful when you create initializers that have the same parameter names as your property.
+
+struct Person {
+    var name: String
+
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+   }
+}
+
+
+
+
+
